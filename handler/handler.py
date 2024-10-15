c7af60b2c192f82067865d676f91112147de1370
Devrank7 <166948437+Devrank7@users.noreply.github.com>
2024-10-15 15:04:31 +0300

    project is prepare 92%

--- handler/handler.py
+++ handler/handler.py
@@ -2,12 +2,16 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from db.db import async_session
+from db.service import UserService
+from util.util import answer_by_lang
 from .middleware.middleware import RegisterCheckMiddleware
 
 router = Router()
 router.message.middleware(RegisterCheckMiddleware())
+user_service = UserService(session=async_session)
 
 
 @router.message(CommandStart())
 async def start_handler(message: Message):
-    await message.answer("hello world")
+    await message.answer(await answer_by_lang('Hello World!', message.chat.id, user_service))
--- util/util.py
+++ util/util.py
@@ -1,6 +1,12 @@
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from googletrans import Translator
+
+from db.enum.enums import Language
+from db.service import UserService
+
+translator = Translator()
 
 
 def build_buttons(values: list[str], prefix: str, separator: int = 2) -> InlineKeyboardBuilder:
@@ -26,3 +32,18 @@ async def exit_handler(message: Message, state: FSMContext, state_name: str) -> 
         await message.answer(f"Exit {state_name} state")
         return True
     return False
+
+
+async def adjust_lang(text: str, lang: Language) -> dict[str, str]:
+    lan = lang.value.strip().lower()
+    detected = translator.detect(text)
+    if lan == detected.lang:
+        return {"text": text, "lang": detected.lang}
+    translated = translator.translate(text, dest=str(lan))
+    return {"text": translated.text, "lang": detected.lang}
+
+
+async def answer_by_lang(text: str,chat_id: int, user_service: UserService) -> str:
+    user = await user_service.read(chat_id)
+    dic = await adjust_lang(text=text, lang=user.lang)
+    return dic['text']
